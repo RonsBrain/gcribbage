deeece7310553bf536db8530af2ec1151b70664b
Ron Martin <ron@ronsbrain.com>
2024-02-08 12:30:58 -0500

    GTK WIP

---
+++ gcribbage_gtk/src/main.rs
@@ -0,0 +1,68 @@
+use gtk::prelude::*;
+use relm4::prelude::*;
+
+struct App {
+    counter: u8,
+}
+
+#[derive(Debug)]
+enum Msg {
+    Increment,
+    Decrement,
+}
+
+#[relm4::component]
+impl SimpleComponent for App {
+    type Init = u8;
+    type Input = Msg;
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            set_title: Some("Simple App"),
+            set_default_size: (300, 100),
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_spacing: 5,
+                set_margin_all: 5,
+
+                gtk::Button {
+                    set_label: "Increment",
+                    connect_clicked[sender] => move |_| { sender.input(Msg::Increment) },
+                },
+                gtk::Button {
+                    set_label: "Decrement",
+                    connect_clicked[sender] => move |_| { sender.input(Msg::Decrement) },
+                },
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("Counter: {}", model.counter),
+                    set_margin_all: 5,
+                },
+            }
+        }
+    }
+
+    fn init(counter: Self::Init, root: &Self::Root, sender: ComponentSender<Self>) -> ComponentParts<Self> {
+        let model = App { counter };
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            Msg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            Msg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let app = RelmApp::new("relm4.example.simple");
+    app.run::<App>(0);
+}
